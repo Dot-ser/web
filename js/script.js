29eae998223ee8a6e7eb9d5d37087a8dc39f6282
Dot-ser <105029178+Dot-ser@users.noreply.github.com>
2022-10-20 15:18:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ const loader = document.querySelector('.loader');
 window.addEventListener('load', () => {
 
     // Making 5s delay in website loading
-    setTimeout(() => {
+    setTimeout((5) => {
 
         loader.classList.add('fadeOut');
-        setTimeout(() => {
+        setTimeout((5) => {
             loader.style.display = 'none';
         }, 1000);
 
